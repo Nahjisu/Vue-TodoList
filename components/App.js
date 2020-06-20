ca912582a077185f80ed1a47ad55937bba021b6e
Nahjisu <51506657+Nahjisu@users.noreply.github.com>
2020-06-20 16:07:34 +0900

    Check List, remove, UI

--- components/App.js
+++ components/App.js
@@ -8,22 +8,38 @@ new Vue({
         }
     },
     methods: {
+
         todoInput() {
             if (this.query !== '') {
                 var obj = { check: false, item: this.query };
                 localStorage.setItem(this.query, JSON.stringify(obj));
+                todoItems.push(obj);
                 this.query = '';
             } else {
                 alert('message');
             }
+        },
 
+        removeList(todoItem, index) {
+            localStorage.removeItem(todoItem.item);
+            this.todoItems.splice(index, 1);    
+        },
+
+        checkCompleted(todoItem, index) {
+            todoItem.check = !todoItem.check;
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+        },
+        clearList() {
+            localStorage.clear();
         }
     },
     created() {
         if (localStorage.length > 0) {
             console.log('hi');
             for (let i = 0; i < localStorage.length; i++) {
-                this.todoItems.push(localStorage.key(i));
+                //console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
             }
         }
     }
